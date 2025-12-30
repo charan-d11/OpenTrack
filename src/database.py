48c1651e85e2fbc2d7aa0e282ac2633e59571471
charan-d11 <mallickdurgacharan188@gmail.com>
2025-12-30 08:04:28 +0530

    Setup OpenTrack: database and menu working

--- src/database.py
+++ src/database.py
@@ -2,3 +2,28 @@ import sqlite3
 
 def connect_db():
     return sqlite3.connect("db/opentrack.db")
+
+def create_tables():
+    conn = connect_db()
+    cur = conn.cursor()
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT NOT NULL,
+        branch TEXT
+    );
+    """)
+
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS tasks (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        student_id INTEGER,
+        task TEXT,
+        status TEXT,
+        FOREIGN KEY (student_id) REFERENCES students(id)
+    );
+    """)
+
+    conn.commit()
+    conn.close()
--- src/main.py
+++ src/main.py
@@ -1,1 +1,21 @@
-print("Welcome to OpenTrack!")
+from database import create_tables
+
+def menu():
+    print("\n1. Add Student")
+    print("2. View Students")
+    print("4. Exit")
+
+def main():
+    print("Welcome to OpenTrack!")
+    create_tables()
+
+    while True:
+        menu()
+        choice = input("Choose: ")
+
+        if choice == "4":
+            print("Exiting OpenTrack...")
+            break
+
+if __name__ == "__main__":
+    main()
